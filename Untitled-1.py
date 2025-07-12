8819d6b1aa8262556b61470a2001d3807b137aa5
jahnavi221FA04556 <jahnavilagadapati9@gmail.com>
2025-07-12 23:46:29 +0530

    find

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+def is_even(number):
+    return number % 2 == 0
+
+# Example usage:
+num = int(input("Enter a number: "))
+if is_even(num):
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
